daef4fd42ae3a458537ff6b427089f43b949b5fd
Dey'Ana Wilson <Deyana033@users.noreply.github.com>
2015-12-06 12:16:56 -0600

    Update node.js

--- node.js
+++ node.js
@@ -1,3 +1,6 @@
+/*referencing http://www.sitepoint.com/creating-a-http-server-in-node-js/ */
+
+
 var http = require("http");
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
