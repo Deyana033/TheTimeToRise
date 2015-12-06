1d3179c63d6889dcbafd7ded9f518b11dd701e22
Dey'Ana Wilson <Deyana033@users.noreply.github.com>
2015-12-06 14:03:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(serveStatic('.'));
 var server = app.listen(3000,function(){
   var host = server.address().address;
   var port = server.address().port;
-  console.log("Static web server listening at http://%s:%s, host, port);
+  console.log("Static web server listening at http://%s:%s", host, port);
 });
