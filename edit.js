595006ffb7f4425aaddd19c4feee66fb7aa7496d
Dey'Ana Wilson <Deyana033@users.noreply.github.com>
2015-12-10 10:38:56 -0600

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,7 +1,19 @@
 function x(d){
-  return d.income
-}
-function y(d){
-  return d.numbtweets
+  return d.income;
 }
 
+function y(d){
+  return d.numbtweets;
+}
+
+function radius(d){
+  
+}
+
+function color(d){
+  return d.state;
+}
+
+function key(d) {
+  return d.name;
+}
